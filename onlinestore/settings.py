8a2f0b6e99e355f9ac7798e9c4cbf4278c311108
ivanbeginer <vaniaiv468@gmail.com>
2025-05-08 01:59:25 +0300

    create products

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import psycopg2
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,13 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'onlinestore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop_postgres',
+        'USER':'postgres',
+        'PASSWORD':'228666',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -117,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT="static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ products/models.py
@@ -0,0 +1,21 @@
+
+from django.db import models
+# Create your models here.
+
+
+class Product(models.Model):
+     name = models.CharField(max_length=255)
+     description = models.TextField()
+     price = models.DecimalField(max_digits=7, decimal_places=2)
+     image = models.ImageField(upload_to='products/')
+     category = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+     def str(self):
+         return [self.name]
+
+class Category(models.Model):
+   name = models.CharField(max_length=255)
+
+   def str(self):
+     return [self.name]
+
